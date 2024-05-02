37303ab57f8fd419f3c0163b568044a425f598cd
jeremyagabriel <jeremyagabriel@gmail.com>
2024-05-02 10:55:11 -0700

    feat: new global context state/actions / usehideiframes hook

--- app/lib/types/global.types.ts
+++ app/lib/types/global.types.ts
@@ -1,3 +1,5 @@
+import type {Settings} from '~/lib/types';
+
 export type Action = {type: string; payload?: any};
 
 export type Dispatch = ({type, payload}: Action) => void;
@@ -9,18 +11,30 @@ export type Modal = {
 
 export interface GlobalState {
   cartOpen: boolean;
+  desktopMenuOpen: boolean;
+  iframesHidden: boolean;
+  mobileMenuOpen: boolean;
   modal: Modal;
+  promobarOpen: boolean;
   searchOpen: boolean;
+  settings: Settings;
   emitter: any;
 }
 
 export interface GlobalActions {
   openCart: () => void;
   closeCart: () => void;
+  openDesktopMenu: () => void;
+  closeDesktopMenu: () => void;
+  openMobileMenu: () => void;
+  closeMobileMenu: () => void;
   openModal: (children: React.ReactNode, props?: Record<string, any>) => void;
   closeModal: () => void;
   openSearch: () => void;
   closeSearch: () => void;
+  togglePromobar: (isOpen: boolean) => void;
+  toggleIframesHidden: (isHidden: boolean) => void;
+  closeAll: () => void;
 }
 
 export interface GlobalContext {
