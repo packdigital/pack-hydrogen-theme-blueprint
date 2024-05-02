cf26178cae716242a9f3010f2981fa0e77e807a3
jeremyagabriel <jeremyagabriel@gmail.com>
2024-05-02 16:18:29 -0700

    chore: add logic to only unload hero image once in view

--- app/sections/Hero/HeroSlide.tsx
+++ app/sections/Hero/HeroSlide.tsx
@@ -1,4 +1,4 @@
-import {useInView} from 'react-intersection-observer';
+import {useEffect, useState} from 'react';
 
 import {Image} from '~/components';
 
@@ -13,16 +13,21 @@ export function HeroSlide({
   slide,
 }: HeroSlideProps) {
   const {image, video} = slide;
-  const {ref, inView} = useInView({
-    rootMargin: '0px',
-    triggerOnce: true,
-  });
+  const [mounted, setMounted] = useState(aboveTheFold && isActiveSlide);
+
   const isVisible =
     (aboveTheFold && isActiveSlide) ||
-    (!aboveTheFold && isActiveSlide && inView);
+    (!aboveTheFold && isActiveSlide && mounted);
+
+  useEffect(() => {
+    if (mounted) return;
+    if (isActiveSlide && !mounted) {
+      setMounted(true);
+    }
+  }, [isActiveSlide]);
 
   return (
-    <div className="relative size-full" ref={ref}>
+    <div className="relative size-full">
       <div className="relative size-full overflow-hidden md:hidden">
         {video?.srcMobile && (
           <HeroVideo
@@ -36,7 +41,7 @@ export function HeroSlide({
           <Image
             data={{
               altText: image.imageMobile.altText || image.alt,
-              url: image.imageMobile.src,
+              url: mounted ? image.imageMobile.src : '',
             }}
             className={`media-fill ${image.positionMobile}`}
             loading={aboveTheFold && isFirstSlide ? 'eager' : 'lazy'}
@@ -58,7 +63,7 @@ export function HeroSlide({
           <Image
             data={{
               altText: image.imageDesktop.altText || image.alt,
-              url: image.imageDesktop.src,
+              url: mounted ? image.imageDesktop.src : '',
             }}
             className={`media-fill ${image.positionDesktop}`}
             loading={aboveTheFold && isFirstSlide ? 'eager' : 'lazy'}
