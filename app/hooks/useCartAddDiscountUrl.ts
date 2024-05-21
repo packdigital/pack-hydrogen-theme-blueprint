9fcd12c28c86f5c96cced3ba340d44d7dc038d9f
Adrian Nuyda <adamnuyda@yahoo.com>
2024-05-21 12:54:35 -0700

    fix: adds discount code via url by storing code in cookies

--- app/hooks/useCartAddDiscountUrl.ts
+++ app/hooks/useCartAddDiscountUrl.ts
@@ -1,6 +1,18 @@
 import {useEffect} from 'react';
 import {useCart} from '@shopify/hydrogen-react';
 
+const getExpirationDate = () => {
+  const now = new Date();
+  const time = now.getTime();
+  const expireTime = time + 1000 * 86400; // 1 day
+  now.setTime(expireTime);
+  return now;
+};
+
+const deleteCookie = (cookieName: string) => {
+  document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
+
 export const useCartAddDiscountUrl = () => {
   const {discountCodesUpdate, id} = useCart();
 
@@ -8,13 +20,28 @@ export const useCartAddDiscountUrl = () => {
     const {search} = window.location;
     const params = new URLSearchParams(search);
 
+    let cookieDiscountCode: boolean | string = false;
+
+    document.cookie?.split(/\s*;\s*/).find((cookie) => {
+      const [key, value] = cookie.split(/\s*=\s*/);
+      if (key !== 'discount_code') return false;
+      cookieDiscountCode = value;
+      return true;
+    });
+
     const discountCode =
       params.get('promo') ||
       params.get('discountCode') ||
-      params.get('discount_code');
+      params.get('discount_code') ||
+      cookieDiscountCode;
+
+    if (discountCode) {
+      document.cookie = `discount_code=${discountCode};expires=${getExpirationDate()?.toUTCString()}`;
+    }
 
     if (discountCode && id) {
       discountCodesUpdate([discountCode]);
+      deleteCookie('discount_code');
     }
   }, [id]);
 };
