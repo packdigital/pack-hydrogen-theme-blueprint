29a94da5351a92766ab9a8f0fecd33880e89664c
Andrew Nguyen <anguy95@gmail.com>
2024-05-28 11:00:23 -0700

    feat: adds in new packages for storefront KPIs

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -2,15 +2,15 @@
 /// <reference types="@shopify/remix-oxygen" />
 /// <reference types="@shopify/oxygen-workers-types" />
 
-import type {Storefront} from '@shopify/hydrogen';
-import type {Pack} from '@pack/hydrogen';
-import type {HydrogenSession} from './server';
+import type { Storefront } from '@shopify/hydrogen';
+import type { Pack } from '@pack/hydrogen';
+import type { HydrogenSession } from './server';
 
 declare global {
   /**
    * A global `process` object is only available during build to access NODE_ENV.
    */
-  const process: {env: {NODE_ENV: 'production' | 'development'}};
+  const process: { env: { NODE_ENV: 'production' | 'development' } };
 
   /**
    * Declare expected Env parameter in fetch handler.
@@ -24,6 +24,7 @@ declare global {
     PUBLIC_STOREFRONT_ID: string;
     PACK_PUBLIC_TOKEN: string;
     PACK_SECRET_TOKEN: string;
+    PACK_STOREFRONT_ID: string;
     PACK_CONTENT_ENVIRONMENT?: string;
     PACK_API_URL?: string;
     PRIVATE_SHOPIFY_CHECKOUT_DOMAIN?: string;
