23785cf3d155855b265c23bc3346e5b30dfd11de
jeremyagabriel <jeremyagabriel@gmail.com>
2024-06-14 15:15:58 -0700

    chore: adds usegeolocation comments

--- app/hooks/useGeoLocation.ts
+++ app/hooks/useGeoLocation.ts
@@ -1,5 +1,28 @@
 import {useRootLoaderData} from '~/hooks';
 
+/**
+ * Get the geolocation object of the buyer
+ * @returns buyer geolocation object
+ * @example
+ * ```js
+ * const geolocation = useGeoLocation();
+ *
+ * // buyer object
+ * // {
+ * //   ip: string;
+ * //   country: string;
+ * //   continent: string;
+ * //   city: string;
+ * //   isEuCountry: boolean;
+ * //   latitude: string;
+ * //   longitude: string;
+ * //   region: string;
+ * //   regionCode: string;
+ * //   timezone: string;
+ * // }
+ * ```
+ */
+
 export const useGeoLocation = () => {
   const {oxygen} = useRootLoaderData();
 
