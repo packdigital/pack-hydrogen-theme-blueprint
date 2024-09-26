2a128b68095f21af03347edabefd1ed9a6eee2ed
Jeremy Gabriel <63984091+jeremyagabriel@users.noreply.github.com>
2024-09-26 11:56:50 -0700

    fix: clear cart buyer identity on log out

--- app/lib/customer/hooks/useCustomerLogOut.ts
+++ app/lib/customer/hooks/useCustomerLogOut.ts
@@ -1,5 +1,6 @@
 import {useCallback} from 'react';
 import {useFetcher, useNavigate} from '@remix-run/react';
+import {useCart} from '@shopify/hydrogen-react';
 
 import {LOGGED_OUT_REDIRECT_TO} from '~/lib/constants';
 import {removeCustomerAccessTokenFromLocalStorage} from '~/lib/customer';
@@ -7,11 +8,14 @@ import {useGlobal, useLocale} from '~/hooks';
 
 export function useCustomerLogOut() {
   const {isPreviewModeEnabled, setPreviewModeCustomer} = useGlobal();
+  const {buyerIdentityUpdate} = useCart();
   const {pathPrefix} = useLocale();
   const navigate = useNavigate();
   const fetcher = useFetcher({key: 'logout'});
 
   const customerLogOut = useCallback(() => {
+    buyerIdentityUpdate({customerAccessToken: null});
+
     /* when in customizer, customer is managed through local storage and
      * global state, instead of session cookies */
     if (isPreviewModeEnabled) {
@@ -20,7 +24,10 @@ export function useCustomerLogOut() {
       return navigate(`${pathPrefix}${LOGGED_OUT_REDIRECT_TO}`);
     }
 
-    fetcher.submit(null, {method: 'POST', action: '/account/logout'});
+    fetcher.submit(null, {
+      method: 'POST',
+      action: `${pathPrefix}/account/logout`,
+    });
   }, []);
 
   return {customerLogOut};
