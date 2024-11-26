b0a6bce98a97a10062db8891f5b52aee3b2174b7
Jeremy Gabriel <63984091+jeremyagabriel@users.noreply.github.com>
2024-11-26 13:20:09 -0800

    chore: add type=module to package.json

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
   watchPaths: ['./public', './.env'],
