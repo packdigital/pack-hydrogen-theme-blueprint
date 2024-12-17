f4831d051155b5bdd712340b924b5a21ebf5e6af
Andrew Nguyen <anguy95@gmail.com>
2024-12-16 16:39:52 -0800

    feat: add in publish ref + timeout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import type {ReactNode} from 'react';
-import {useCallback} from 'react';
-
+import {useCallback, useEffect, useState, useRef} from 'react';
 import {useAnalytics} from '@shopify/hydrogen';
 import {PackTestProvider} from '@pack/hydrogen';
 
@@ -19,18 +18,61 @@ import {
   useScrollToHashOnNavigation,
   useSetViewportHeightCssVar,
 } from '~/hooks';
-
 export function Layout({children}: {children: ReactNode}) {
   const {mainPaddingTopClass} = usePromobar();
   useCartAddDiscountUrl();
   useScrollToHashOnNavigation();
   useSetViewportHeightCssVar();
 
+  const [hasUserConsent, setHasUserConsent] = useState<boolean>(false);
+  const {publish} = useAnalytics();
+
+  const publishRef = useRef(publish);
+
+  useEffect(() => {
+    publishRef.current = publish;
+  }, [publish]);
+
+  const handleTestExpose = (test: any) => {
+    if (hasUserConsent) {
+      setTimeout(() => {
+        publishRef?.current?.(AnalyticsEvent.EXPERIMENT_EXPOSED, {test});
+      }, 1000);
+    }
+  };
+
+  // https://shopify.dev/docs/api/customer-privacy#use-an-event-listener
+  useEffect(() => {
+    const handleConsentChange = (event: any) => {
+      const hasAnalyticsAllowed = event?.detail?.analyticsAllowed || false;
+
+      setHasUserConsent(hasAnalyticsAllowed);
+      console.log('Consent collected:', event.detail); // Log the event details if needed
+
+      if (hasAnalyticsAllowed) {
+        publish?.(AnalyticsEvent.EXPERIMENT_EXPOSED, {test: {adf: 124}});
+      }
+    };
+
+    // Add event listener for 'visitorConsentCollected'
+    document.addEventListener('visitorConsentCollected', handleConsentChange);
+
+    return () => {
+      // Clean up the event listener on unmount
+      document.removeEventListener(
+        'visitorConsentCollected',
+        handleConsentChange,
+      );
+    };
+  }, []);
+
   return (
     <>
       <Analytics />
-
-      <TestLayoutComponent>
+      <PackTestProvider
+        hasUserConsent={hasUserConsent}
+        testExposureCallback={handleTestExpose}
+      >
         <div
           className="flex h-[var(--viewport-height)] flex-col"
           data-comp={Layout.displayName}
@@ -55,27 +97,9 @@ export function Layout({children}: {children: ReactNode}) {
 
           <Modal />
         </div>
-      </TestLayoutComponent>
+      </PackTestProvider>
     </>
   );
 }
-
-const TestLayoutComponent = ({children}: {children: ReactNode}) => {
-  const {publish} = useAnalytics();
-
-  const handleTestExpose = useCallback(
-    (test: any) => {
-      console.log('==== I HAVE BEEN EXPOSED!!!', test);
-      publish(AnalyticsEvent.EXPERIMENT_EXPOSED, {test});
-    },
-    [publish],
-  );
-
-  return (
-    <PackTestProvider testExposureCallback={handleTestExpose}>
-      {children}
-    </PackTestProvider>
-  );
-};
 
 Layout.displayName = 'Layout';
