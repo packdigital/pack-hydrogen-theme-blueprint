b43f120a314538d46e838d351d1d6f8696d551af
DynamikArray <dynamikarray@gmail.com>
2025-04-15 21:19:30 -0400

    feature(Theme): Adds Blue CSS, our Logo, hides Color Swatches

--- app/components/Product/Product.tsx
+++ app/components/Product/Product.tsx
@@ -1,15 +1,15 @@
-import {useEffect, useMemo} from 'react';
 import {useProduct} from '@shopify/hydrogen-react';
+import {useEffect, useMemo} from 'react';
 
-import {COLOR_OPTION_NAME} from '~/lib/constants';
-import {useLocale, useParsedProductMetafields, useSettings} from '~/hooks';
-import type {SelectedVariant} from '~/lib/types';
-
+import type {ProductProps} from './Product.types';
 import {ProductDetails} from './ProductDetails';
-import {ProductMetafields} from './ProductMetafields';
 import {ProductHeader} from './ProductHeader';
 import {ProductMedia} from './ProductMedia';
-import type {ProductProps} from './Product.types';
+import {ProductMetafields} from './ProductMetafields';
+
+import {useLocale, useParsedProductMetafields, useSettings} from '~/hooks';
+import {COLOR_OPTION_NAME} from '~/lib/constants';
+import type {SelectedVariant} from '~/lib/types';
 
 export function Product({
   initialSelectedVariant,
@@ -61,7 +61,14 @@ export function Product({
     const updatedUrl = `${origin}${pathPrefix}/products/${product.handle}?${params}`;
 
     window.history.replaceState(window.history.state, '', updatedUrl);
-  }, [isModalProduct, isSectionProduct, product.handle, selectedVariant?.id]);
+  }, [
+    isModalProduct,
+    isSectionProduct,
+    pathPrefix,
+    product.handle,
+    product.variants?.nodes.length,
+    selectedVariant,
+  ]);
 
   const stickyPromobar =
     header?.promobar?.enabled && !header?.promobar?.autohide;
@@ -122,7 +129,7 @@ export function Product({
                 selectedVariant={selectedVariant}
               />
 
-              <ProductMetafields product={product} />
+              {/* <ProductMetafields product={product} /> */}
             </div>
           </div>
         </div>
