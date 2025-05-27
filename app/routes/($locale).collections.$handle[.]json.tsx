5b8bfd84dda3470827c76484922762f08eaa9ef0
Adrian Nuyda <adamnuyda@yahoo.com>
2025-05-27 13:11:38 -0700

    feat: adds json routes

---
+++ app/routes/($locale).collections.$handle[.]json.tsx
@@ -0,0 +1,32 @@
+import {json} from '@shopify/remix-oxygen';
+import {getPaginationVariables} from '@shopify/hydrogen';
+import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+
+import {COLLECTION_QUERY} from '~/data/graphql/storefront/collection';
+import {routeHeaders} from '~/data/cache';
+export const headers = routeHeaders;
+
+export async function loader({params, context, request}: LoaderFunctionArgs) {
+  const {handle} = params;
+  const {storefront} = context;
+
+  if (!handle) throw new Response(null, {status: 404});
+
+  const paginationVariables = getPaginationVariables(request, {
+    pageBy: 24,
+  });
+
+  const collection = await storefront.query(COLLECTION_QUERY, {
+    variables: {
+      handle,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+      ...paginationVariables,
+    },
+    cache: storefront.CacheShort(),
+  });
+
+  if (!collection) throw new Response(null, {status: 404});
+
+  return json(collection);
+}
---
+++ app/routes/($locale).products.$handle[.]json.tsx
@@ -0,0 +1,31 @@
+import {json} from '@shopify/remix-oxygen';
+import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+
+import {getSelectedProductOptions} from '~/lib/products.server';
+import {PRODUCT_QUERY} from '~/data/graphql/storefront/product';
+import {routeHeaders} from '~/data/cache';
+
+export const headers = routeHeaders;
+
+export async function loader({params, context, request}: LoaderFunctionArgs) {
+  const {handle} = params;
+  const {storefront} = context;
+
+  const selectedOptions = await getSelectedProductOptions({
+    handle,
+    context,
+    request,
+  });
+
+  const product = await storefront.query(PRODUCT_QUERY, {
+    variables: {
+      handle,
+      selectedOptions,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+    },
+    cache: storefront.CacheShort(),
+  });
+
+  return json(product);
+}
