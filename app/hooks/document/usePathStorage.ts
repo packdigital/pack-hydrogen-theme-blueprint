1f62785d240b3d1ce0f56132227eb8d927a743aa
Jeremy Gabriel <63984091+jeremyagabriel@users.noreply.github.com>
2025-12-02 11:07:28 -0800

    chore: wrap session storage set in try catch

--- app/hooks/document/usePathStorage.ts
+++ app/hooks/document/usePathStorage.ts
@@ -6,26 +6,34 @@ export function usePathStorage() {
 
   // Set previous path and current path in session storage
   useEffect(() => {
-    const currentPath = sessionStorage.getItem('CURRENT_PATH') || '';
-    if (pathname === currentPath) {
-      sessionStorage.setItem('PREVIOUS_PATH_INLC_REFRESH', pathname);
-      return;
+    try {
+      const currentPath = sessionStorage.getItem('CURRENT_PATH') || '';
+      if (pathname === currentPath) {
+        sessionStorage.setItem('PREVIOUS_PATH_INLC_REFRESH', pathname);
+        return;
+      }
+      sessionStorage.setItem('PREVIOUS_PATH', currentPath);
+      sessionStorage.setItem('PREVIOUS_PATH_INLC_REFRESH', currentPath);
+      sessionStorage.setItem('CURRENT_PATH', pathname);
+    } catch (error) {
+      console.error('Error setting path storage:', error);
     }
-    sessionStorage.setItem('PREVIOUS_PATH', currentPath);
-    sessionStorage.setItem('PREVIOUS_PATH_INLC_REFRESH', currentPath);
-    sessionStorage.setItem('CURRENT_PATH', pathname);
   }, [pathname]);
 
   // Set previous url and current url in session storage
   useEffect(() => {
-    const url = window.location.href;
-    const currentUrl = sessionStorage.getItem('CURRENT_URL') || '';
-    if (url === currentUrl) {
-      sessionStorage.setItem('PREVIOUS_URL_INLC_REFRESH', url);
-      return;
+    try {
+      const url = window.location.href;
+      const currentUrl = sessionStorage.getItem('CURRENT_URL') || '';
+      if (url === currentUrl) {
+        sessionStorage.setItem('PREVIOUS_URL_INLC_REFRESH', url);
+        return;
+      }
+      sessionStorage.setItem('PREVIOUS_URL', currentUrl);
+      sessionStorage.setItem('PREVIOUS_URL_INLC_REFRESH', currentUrl);
+      sessionStorage.setItem('CURRENT_URL', url);
+    } catch (error) {
+      console.error('Error setting url storage:', error);
     }
-    sessionStorage.setItem('PREVIOUS_URL', currentUrl);
-    sessionStorage.setItem('PREVIOUS_URL_INLC_REFRESH', currentUrl);
-    sessionStorage.setItem('CURRENT_URL', url);
   }, [pathname]);
 }
