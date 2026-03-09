e21e9b2bf3da948a2abff2f5037a1118d5fccb93
Jeremy Gabriel <63984091+jeremyagabriel@users.noreply.github.com>
2026-03-09 14:59:04 -0700

    chore: removes redundant cart analytic actions

--- app/hooks/cart/useCart.ts
+++ app/hooks/cart/useCart.ts
@@ -1,16 +1,9 @@
 import {useCallback} from 'react';
 import {CartForm, flattenConnection, useAnalytics} from '@shopify/hydrogen';
 import {useCart as useClientSideCart} from '@shopify/hydrogen-react';
-import equal from 'fast-deep-equal';
-import type {
-  Cart,
-  CartBuyerIdentityInput,
-  CartLine,
-  CartLineInput,
-} from '@shopify/hydrogen/storefront-api-types';
+import type {CartBuyerIdentityInput} from '@shopify/hydrogen/storefront-api-types';
 
 import {useCartContext} from '~/contexts/CartProvider/useCartContext';
-import {AnalyticsEvent} from '~/components/Analytics/constants';
 import type {CartActionData, CartWithActions} from '~/lib/types';
 
 import {useRootLoaderData} from '../useRootLoaderData';
@@ -64,7 +57,7 @@ export const useCart = (): CartWithActions => {
       mutationQueue = mutationQueue.then(mutation, mutation);
       return mutationQueue;
     },
-    [],
+    [setCart, setError, setStatus],
   );
 
   const cartCreate: CartWithActions['cartCreate'] = useCallback(
@@ -74,7 +67,7 @@ export const useCart = (): CartWithActions => {
       formData.append('cart', JSON.stringify(cartInput));
       return getCartActionData(formData);
     },
-    [],
+    [getCartActionData],
   );
 
   const linesAdd: CartWithActions['linesAdd'] = useCallback(
@@ -82,32 +75,9 @@ export const useCart = (): CartWithActions => {
       const formData = new FormData();
       formData.append('action', CartForm.ACTIONS.LinesAdd);
       formData.append('lines', JSON.stringify(lines));
-      const data = await getCartActionData(formData);
-      if (data?.cart) {
-        lines.forEach((lineInput) => {
-          const prevLine =
-            flattenConnection(cart?.lines).find((prevCartLine) => {
-              return compareInputWithLine(lineInput, prevCartLine as CartLine);
-            }) || null;
-          const currentLine =
-            flattenConnection(data.cart?.lines).find((currentCartLine) => {
-              return compareInputWithLine(
-                lineInput,
-                currentCartLine as CartLine,
-              );
-            }) || null;
-          publish(AnalyticsEvent.PRODUCT_ADD_TO_CART, {
-            cart: data.cart,
-            prevCart: cart,
-            currentLine,
-            prevLine,
-            shop,
-          });
-        });
-      }
-      return data;
+      return getCartActionData(formData);
     },
-    [cart, publish, shop],
+    [cart, getCartActionData, publish, shop],
   );
 
   const linesUpdate: CartWithActions['linesUpdate'] = useCallback(
@@ -115,39 +85,9 @@ export const useCart = (): CartWithActions => {
       const formData = new FormData();
       formData.append('action', CartForm.ACTIONS.LinesUpdate);
       formData.append('lines', JSON.stringify(lines));
-      const data = await getCartActionData(formData);
-      if (data?.cart) {
-        lines.forEach((lineInput) => {
-          const prevLine =
-            flattenConnection(cart?.lines).find((prevCartLine) => {
-              return prevCartLine.id === lineInput.id;
-            }) || null;
-          const currentLine =
-            flattenConnection(data.cart?.lines).find((currentCartLine) => {
-              return currentCartLine.id === lineInput.id;
-            }) || null;
-          if ((currentLine?.quantity || 0) > (prevLine?.quantity || 0)) {
-            publish(AnalyticsEvent.PRODUCT_ADD_TO_CART, {
-              cart: data.cart,
-              prevCart: cart,
-              currentLine,
-              prevLine,
-              shop,
-            });
-          } else if ((currentLine?.quantity || 0) < (prevLine?.quantity || 0)) {
-            publish(AnalyticsEvent.PRODUCT_REMOVED_FROM_CART, {
-              cart: data.cart,
-              prevCart: cart,
-              currentLine,
-              prevLine,
-              shop,
-            });
-          }
-        });
-      }
-      return data;
+      return getCartActionData(formData);
     },
-    [cart, publish, shop],
+    [cart, getCartActionData, publish, shop],
   );
 
   const linesRemove: CartWithActions['linesRemove'] = useCallback(
@@ -155,46 +95,32 @@ export const useCart = (): CartWithActions => {
       const formData = new FormData();
       formData.append('action', CartForm.ACTIONS.LinesRemove);
       formData.append('lineIds', JSON.stringify(lineIds));
-      const data = await getCartActionData(formData);
-      if (data?.cart) {
-        lineIds.forEach((lineId) => {
-          const prevLine =
-            flattenConnection(cart?.lines).find((prevCartLine) => {
-              return prevCartLine.id === lineId;
-            }) || null;
-          const currentLine =
-            flattenConnection(data.cart?.lines).find((currentCartLine) => {
-              return currentCartLine.id === lineId;
-            }) || null;
-          publish(AnalyticsEvent.PRODUCT_REMOVED_FROM_CART, {
-            cart: data.cart,
-            prevCart: cart,
-            currentLine,
-            prevLine,
-            shop,
-          });
-        });
-      }
-      return data;
+      return getCartActionData(formData);
     },
-    [cart, publish, shop],
+    [cart, getCartActionData, publish, shop],
   );
 
   const discountCodesUpdate: CartWithActions['discountCodesUpdate'] =
-    useCallback(async (discountCodes) => {
-      const formData = new FormData();
-      formData.append('action', CartForm.ACTIONS.DiscountCodesUpdate);
-      formData.append('discountCodes', JSON.stringify(discountCodes));
-      return getCartActionData(formData);
-    }, []);
+    useCallback(
+      async (discountCodes) => {
+        const formData = new FormData();
+        formData.append('action', CartForm.ACTIONS.DiscountCodesUpdate);
+        formData.append('discountCodes', JSON.stringify(discountCodes));
+        return getCartActionData(formData);
+      },
+      [getCartActionData],
+    );
 
   const cartAttributesUpdate: CartWithActions['cartAttributesUpdate'] =
-    useCallback(async (attributes) => {
-      const formData = new FormData();
-      formData.append('action', CartForm.ACTIONS.AttributesUpdateInput);
-      formData.append('attributes', JSON.stringify(attributes));
-      return getCartActionData(formData);
-    }, []);
+    useCallback(
+      async (attributes) => {
+        const formData = new FormData();
+        formData.append('action', CartForm.ACTIONS.AttributesUpdateInput);
+        formData.append('attributes', JSON.stringify(attributes));
+        return getCartActionData(formData);
+      },
+      [getCartActionData],
+    );
 
   const buyerIdentityUpdate: CartWithActions['buyerIdentityUpdate'] =
     useCallback(
@@ -206,7 +132,7 @@ export const useCart = (): CartWithActions => {
         formData.append('buyerIdentity', JSON.stringify(buyerIdentity));
         return getCartActionData(formData);
       },
-      [],
+      [getCartActionData],
     );
 
   const noteUpdate: CartWithActions['noteUpdate'] = useCallback(
@@ -216,7 +142,7 @@ export const useCart = (): CartWithActions => {
       formData.append('note', note);
       return getCartActionData(formData);
     },
-    [],
+    [getCartActionData],
   );
 
   return (
@@ -238,30 +164,4 @@ export const useCart = (): CartWithActions => {
           status,
         }
   ) as CartWithActions;
-};
-
-const compareInputWithLine = (input: CartLineInput, line: CartLine) => {
-  const inputAttrLength = input.attributes ? input.attributes.length : 0;
-  const lineAttrLength = line.attributes ? line.attributes.length : 0;
-  if (inputAttrLength !== lineAttrLength) return false;
-  if (inputAttrLength || lineAttrLength) {
-    const inputAttributesByKey = input.attributes?.reduce(
-      (acc: Record<string, string>, attr) => {
-        acc[attr.key] = attr.value;
-        return acc;
-      },
-      {},
-    );
-    const lineAttributesByKey = line.attributes?.reduce(
-      (acc: Record<string, string>, attr) => {
-        acc[attr.key] = attr.value;
-        return acc;
-      },
-      {},
-    );
-    if (!equal(inputAttributesByKey, lineAttributesByKey)) return false;
-  }
-  if (input.sellingPlanId !== line.sellingPlanAllocation?.sellingPlan?.id)
-    return false;
-  return line.merchandise.id === input.merchandiseId;
 };
